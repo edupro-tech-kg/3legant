7cbff16f6272fe94895c0b90d2619777c7da61b6
adskiydrochila54 <bloopersgag@gmail.com>
2025-12-18 15:42:04 +0600

    fixing

--- config/urls.py
+++ config/urls.py
@@ -29,4 +29,5 @@ urlpatterns = [
     path('api/auth/token/refresh/', TokenRefreshView.as_view()),
     path('api/auth/', include('accounts.urls')),
     path('api/newsletter/', include('newsletter.urls')),
+    path('api/cart', include('cart.urls')),
 ]
